900631d4ca37f3fc06de9035da27a073d6d7b4cc
SAHIL13928 <148043150+SAHIL13928@users.noreply.github.com>
2025-12-31 16:36:02 +0530

    cors changes for vercel integrations

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { clerkMiddleware, requireAuth } from '@clerk/express'
-import bodyParser from 'body-parser';
+import { clerkMiddleware } from '@clerk/express';
 import dotenv from 'dotenv/config';
 import aiRouter from './routes/aiRoutes.js';
 import userRouter from './routes/userRouter.js';
@@ -10,20 +9,27 @@ import connectCloudinary from './configs/cloudinary.js';
 const app = express();
 
 await connectCloudinary();
-app.use(cors());
+
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://your-frontend.vercel.app'
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
 
 app.use('/api/ai', aiRouter);
 app.use('/api/user', userRouter);
 
-app.get("/", (req, res) => 
-  res.status(200).send("Server is running")
-)
-
+app.get('/', (req, res) => {
+  res.status(200).send('Server is running');
+});
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log('Server is running on port' , PORT);
+  console.log('Server is running on port', PORT);
 });
