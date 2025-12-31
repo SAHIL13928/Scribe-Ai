1cedd787706c8bd4518221dc73494187da48a15f
SAHIL13928 <148043150+SAHIL13928@users.noreply.github.com>
2025-12-31 17:12:58 +0530

    vercel.json added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import { clerkMiddleware } from '@clerk/express';
+import { clerkMiddleware, requireAuth } from '@clerk/express'
+import bodyParser from 'body-parser';
 import dotenv from 'dotenv/config';
 import aiRouter from './routes/aiRoutes.js';
 import userRouter from './routes/userRouter.js';
@@ -9,32 +10,20 @@ import connectCloudinary from './configs/cloudinary.js';
 const app = express();
 
 await connectCloudinary();
-
-const corsOptions = {
-  origin: [
-    'http://localhost:5173',
-    'https://mirage-q8fskjhbp-sahil-kumars-projects-f11074dd.vercel.app'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-};
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
-
+app.use(cors());
 app.use(express.json());
-app.use(clerkMiddleware());
+app.use(clerkMiddleware())
 
 app.use('/api/ai', aiRouter);
 app.use('/api/user', userRouter);
 
-app.get('/', (req, res) => {
-  res.status(200).send('Server is running');
-});
+app.get("/", (req, res) => 
+  res.status(200).send("Server is running")
+)
+
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log('Server is running on port', PORT);
+  console.log('Server is running on port' , PORT);
 });
