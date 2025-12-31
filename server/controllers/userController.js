8c68fa2ef134d97f1541199d008189ae22356c75
SAHIL13928 <148043150+SAHIL13928@users.noreply.github.com>
2025-12-31 18:24:50 +0530

    dashboard ready to fetch real time creations

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,7 +3,7 @@ import sql from "../configs/db.js";
 
 export const getUserCreations = async (req, res) => {
   try {
-    const { userId } = req.auth();
+    const { userId } = req.auth;
 
     const creations = await sql`
       SELECT *
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -4,7 +4,7 @@ import { getUserCreations, toggleLikeCreation } from "../controllers/userControl
 
 const userRouter = express.Router();
 
-userRouter.get("/get-published-creations",auth, getUserCreations);
+userRouter.get("/save-creations",auth, getUserCreations);
 userRouter.post("/toggle-like-creations",auth, toggleLikeCreation);
 //userRouter.get("/get-published-creations",auth, getPublishedCreations);
 
